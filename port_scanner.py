d39c1d2a4ce3dfd25c46133adc4581039b2ed821
ARFAT HUSSAIN <syedarfat0305@gmail.com>
2026-03-16 15:41:06 +0530

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,1 +1,21 @@
+import socket
+import threading
 
+target = input("Enter target IP or domain: ")
+
+print(f"\nScanning target: {target}\n")
+
+def scan_port(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Port {port}: Open")
+
+    s.close()
+
+for port in range(1, 1025):
+    thread = threading.Thread(target=scan_port, args=(port,))
+    thread.start()
